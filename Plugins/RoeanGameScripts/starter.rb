b1e8951843dcd41c55fac54ed5e412287d1935ce
RoeanNijima <144230311+RoeanNijima@users.noreply.github.com>
2025-10-29 00:10:20 +0000

    starter pregen (really easy tbh)

--- Plugins/RoeanGameScripts/starter.rb
+++ Plugins/RoeanGameScripts/starter.rb
@@ -32,12 +32,43 @@ POKE_STARTERS = [
 ]
 
 def beginStarterSelect()
-  while true do
-    starter_index = pbMessage("Choose a Pokemon.", POKE_STARTERS)
-    choice = pbMessage("\\bDo I really want " + POKE_STARTERS[starter_index] + "?", ["YES", "NO"])
+  viewport = Viewport.new(0, 0, Graphics.width, Graphics.height)
+  viewport.z = 99999
+  sprite = PokemonSprite.new(viewport)
+  sprite.visible = false
 
-    if choice == 0 then break end
+  starters = POKE_STARTERS.map { |s| Pokemon.new(s.to_sym, 5) }
+  chosen_index = nil
+
+  sprite_x = Graphics.width / 2
+  sprite_y = Graphics.height / 2
+
+  loop do
+    starter_index = pbMessage("Choose a Pokémon.", POKE_STARTERS)
+    chosen_pokemon = starters[starter_index]
+
+    sprite.setPokemonBitmap(chosen_pokemon)
+    sprite.ox = sprite.bitmap.width / 2
+    sprite.oy = sprite.bitmap.height / 2
+    sprite.x = sprite_x
+    sprite.y = sprite_y
+    sprite.visible = true
+
+    pbSEPlay("Shiny sparkle") if chosen_pokemon.shiny?
+    pbMessage("There is something different about this pokemon...") if chosen_pokemon.shiny?
+
+    Graphics.update
+    Input.update
+
+    choice = pbMessage("\\bDo I really want #{chosen_pokemon.name}?", ["YES", "NO"])
+    if choice == 0
+      chosen_index = starter_index
+      break
+    end
+    sprite.visible = false
   end
 
-  pbAddPokemon(POKE_STARTERS[starter_index].to_sym, 5)
+  pbAddPokemon(starters[chosen_index]) if chosen_index
+  sprite.dispose
+  viewport.dispose
 end
